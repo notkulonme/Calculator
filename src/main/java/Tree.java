a7c7edd40067db79eae629801bf55512b0a037c3
notkulonme <nricsi006@gmail.com>
2025-05-24 19:31:13 +0200

    creating Tree structure

---
+++ src/main/java/Tree.java
@@ -0,0 +1,22 @@
+﻿import hu.notkulonme.Token;
+
+public class Tree {
+    Tree leftChild;
+    Tree rightChild;
+    Tree parent;
+    Token value;
+
+    public Tree(Tree leftChild, Tree rightChild, Token value){
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+        this.parent = null;
+        this.value = value;
+    }
+
+    public Tree(Tree leftChild, Tree rightChild, Tree parent, Token value){
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+        this.parent = parent;
+        this.value = value;
+    }
+}
