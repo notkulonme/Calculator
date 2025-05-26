f714b33409889b99cecab548ec23bb149538d558
notkulonme <nricsi006@gmail.com>
2025-05-26 23:15:37 +0200

    creating Parser.kt

--- src/main/kotlin/Parser.kt
+++ src/main/kotlin/Parser.kt
@@ -1,5 +1,17 @@
 ﻿package hu.notkulonme
 
+import InputError
+import Tree
+import hu.notkulonme.tokenizer.Token
+import hu.notkulonme.tokenizer.TokenType
+
 class Parser {
+    @Throws(InputError::class)
+    fun parseIntoTree(tokenList: List<Token>):Tree{
+        if (tokenList.count{ it.type == TokenType.INVALID_NUMBER } > 0)
+            throw InputError(tokenList.stream().filter { it.type == TokenType.INVALID_NUMBER }.map { it.value }.toList())
+        return Tree(null, null)
+    }
+
 
 }
