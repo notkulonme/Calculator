375597f3dc5c0a3ac6127d53067575cf5b2cbdfa
notkulonme <nricsi006@gmail.com>
2025-05-28 14:01:00 +0200

    start adding token list validition

--- src/main/kotlin/Parser.kt
+++ src/main/kotlin/Parser.kt
@@ -6,12 +6,23 @@ import hu.notkulonme.tokenizer.Token
 import hu.notkulonme.tokenizer.TokenType
 
 class Parser {
-    @Throws(InputError::class)
+    lateinit var tokenList: List<Token>
     fun parseIntoTree(tokenList: List<Token>):Tree{
-        if (tokenList.count{ it.type == TokenType.INVALID_NUMBER } > 0)
-            throw InputError(tokenList.stream().filter { it.type == TokenType.INVALID_NUMBER }.map { it.value }.toList())
+        this.tokenList = tokenList
+        validateTokenList()
         return Tree(null, null)
     }
 
+    fun validateTokenList(){
+        if (tokenList.count{ it.type == TokenType.INVALID_NUMBER } > 0)
+            throw InputError(tokenList.stream().filter { it.type == TokenType.INVALID_NUMBER }.map { it.value }.toList())
+        if (tokenList.count{it.type == TokenType.NOT_TOKEN } > 0)
+            throw InputError(TokenType.NOT_TOKEN)
+        val operatorIndexList = mutableListOf<Int>()
+        for ((index, token) in tokenList.withIndex()){
+            if (token.type == TokenType.OPERATOR)
+                operatorIndexList.add(index)
+        }
 
+    }
 }
