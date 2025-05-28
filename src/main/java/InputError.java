70a939c41a757d40778af2226744634afd2a99ab
notkulonme <nricsi006@gmail.com>
2025-05-28 13:59:39 +0200

    adding other type of error

--- src/main/java/InputError.java
+++ src/main/java/InputError.java
@@ -1,7 +1,12 @@
+import hu.notkulonme.tokenizer.TokenType;
+
 import java.util.List;
 
 public class InputError extends RuntimeException{
     public InputError(List<String> wrongNumbers){
         super("Wrongly formated number(s): " + String.join("; ", wrongNumbers));
+    }
+    public InputError(TokenType tokenType){
+        super("Invalid character(s)");
     }
 }
