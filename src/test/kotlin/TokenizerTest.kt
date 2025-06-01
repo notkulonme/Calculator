faec23ca1e92600cdc64f18dcb0a96194ed6b448
notkulonme <nricsi006@gmail.com>
2025-06-01 13:20:36 +0200

    adding test for Tokenizer class

--- src/test/kotlin/TokenizerTest.kt
+++ src/test/kotlin/TokenizerTest.kt
@@ -1,5 +1,30 @@
-﻿import org.junit.jupiter.api.Assertions.*
+﻿import hu.notkulonme.tokenizer.Token
+import hu.notkulonme.tokenizer.TokenType
+import hu.notkulonme.tokenizer.Tokenizer
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 class TokenizerTest {
+    val tokenizer = Tokenizer()
 
+    @Test
+    fun testParenthesis(){
+        val result = tokenizer.tokenize("((3)/)")
+        val expected = mutableListOf<Token>(Token("(", TokenType.PARENTHESIS),Token("(", TokenType.PARENTHESIS),Token("3", TokenType.NUMBER),Token(")", TokenType.PARENTHESIS),Token("/", TokenType.OPERATOR),Token(")", TokenType.PARENTHESIS))
+        assertIterableEquals(expected, result)
+    }
+
+    @Test
+    fun  testNumbers(){
+        val result = tokenizer.tokenize("32.5  32..5" )
+        val expected = mutableListOf<Token>(Token("32.5", TokenType.NUMBER), Token("  ", TokenType.WHITESPACE),Token("32..5", TokenType.INVALID_NUMBER))
+        assertIterableEquals(expected, result)
+    }
+
+    @Test
+    fun  testOperators(){
+        val result = tokenizer.tokenize("+-/+3*" )
+        val expected = mutableListOf<Token>(Token("+", TokenType.OPERATOR), Token("-", TokenType.OPERATOR),Token("/", TokenType.OPERATOR),Token("+", TokenType.OPERATOR),Token("3", TokenType.NUMBER),Token("*", TokenType.OPERATOR))
+        assertIterableEquals(expected, result)
+    }
 }
